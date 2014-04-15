0b00dcbc234f2b2e681b03b472db8ccf96f7f5cd
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-04-15 21:50:56 +0900

    connect to mysql database

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'test_user',
+  password: process.env.DB_PASS || 'test_password',
+  database: process.env.DB_NAME || 'test_db'
+});
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
