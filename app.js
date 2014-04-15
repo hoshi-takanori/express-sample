2b0ffa38f6dad10b854819a3a55a9fb2273bd976
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-04-15 20:57:33 +0900

    add views/index.jade

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
-  res.send('Hello, World!');
+  res.render('index', { title: 'Express Sample' });
 });
 
 app.listen(3000);
