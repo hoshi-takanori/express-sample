b9a5a2bc1f16bcc920b9669bce3ff1690d4f0e01
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-04-15 20:59:18 +0900

    add public/css/style.css

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.render('index', { title: 'Express Sample' });
 });
