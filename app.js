a0de8a8f16ef2ecb03db86d7a6539ee7c634da82
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-06-24 15:20:39 +0900

    print access log

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+if (app.get('env') == 'production') {
+  var fs = require('fs');
+  var stream = fs.createWriteStream(__dirname + '/log.txt', { flags: 'a' });
+  app.use(morgan({ stream: stream }));
+} else {
+  //app.use(morgan('short'));
+  app.use(morgan({ format: 'dev', immediate: true }));
+}
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
