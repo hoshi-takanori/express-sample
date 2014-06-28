0bf52266aba86eeda6c42bfcec7e50d9e1aaa124
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-06-28 21:25:18 +0900

    replace raw cookie with session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
 
 app.set('view engine', 'jade');
@@ -19,25 +20,23 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-//app.use(cookieParser('cookie secret'));
+app.use(session({ secret: process.env.SESSION_SECRET || 'session secret' }));
 
 app.get('/', function(req, res) {
-  var username = req.cookies.username;
-  //var username = req.signedCookies.username;
+  var username = req.session.username;
   res.render('index', { title: 'Express Sample', username: username });
 });
 
 app.post('/', function(req, res) {
   if (req.param('logout')) {
-    res.clearCookie('username');
+    req.session.destroy();
     res.redirect('/');
     return;
   }
   var username = req.param('username');
   var password = req.param('password');
   if (username == password) {
-    res.cookie('username', username);
-    //res.cookie('username', username, { signed: true });
+    req.session.username = req.body.username;
     res.redirect('/');
   } else {
     res.render('index', { title: 'Express Sample', error: 'Unknown username or password.' });
