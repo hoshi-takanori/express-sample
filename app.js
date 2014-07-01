c74545b7aec05d41ded534c970a4429fd9f009a8
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-07-01 15:27:10 +0900

    add member-only pages

--- app.js
+++ app.js
@@ -58,25 +58,71 @@ if (process.env.DEBUG_SESSION) {
 }
 
 app.get('/', function (req, res) {
-  var username = req.session.username;
-  res.render('index', { title: 'Express Sample', username: username });
+  if (! req.session.username) {
+    res.render('index', { title: 'Express Sample' });
+  } else {
+    res.render('index', {
+      title: 'Express Sample',
+      username: req.session.username,
+      links: [
+        { href: '/aaa', text: 'AAA' },
+        { href: '/bbb', text: 'BBB' },
+        { href: '/ccc', text: 'CCC' }
+      ]
+    });
+  }
 });
 
-app.post('/login', function (req, res) {
+app.get('/login', function (req, res) {
+  if (! req.session.username) {
+    res.render('index', { title: 'Express Sample', error: 'This page is for members only. Please login.' });
+  } else {
+    res.redirect('/');
+  }
+});
+
+function checkPassword(username, password, fn) {
+  fn(null, username == password);
+}
+
+app.post('/login', function (req, res, next) {
   var username = req.param('username');
   var password = req.param('password');
-  if (username == password) {
-    req.session.username = username;
-    res.redirect('/');
-  } else {
-    res.render('index', { title: 'Express Sample', error: 'Unknown username or password.' });
-  }
+  checkPassword(username, password, function (err, result) {
+    if (err) return next(err);
+    if (result) {
+      req.session.username = username;
+      res.redirect('/');
+    } else {
+      res.render('index', { title: 'Express Sample', error: 'Unknown username or password.' });
+    }
+  });
 });
 
 app.post('/logout', function (req, res) {
   req.session.destroy();
   res.clearCookie(cookieName);
   res.redirect('/');
+});
+
+app.use(function (req, res, next) {
+  if (! req.session.username) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+});
+
+app.get('/aaa', function (req, res) {
+  res.render('index', { title: 'AAA', message: 'Welcome to AAA.' });
+});
+
+app.get('/bbb', function (req, res) {
+  res.render('index', { title: 'BBB', message: 'Welcome to BBB.' });
+});
+
+app.get('/ccc', function (req, res) {
+  res.render('index', { title: 'CCC', message: 'Welcome to CCC.' });
 });
 
 app.listen(process.env.PORT || 3000);
